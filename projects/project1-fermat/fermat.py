71b81fb2de38663c7a4e622bb65dc7dd5bddc25a
aonstott <aonstott32@gmail.com>
2024-03-12 18:34:20 -0600

    p4

--- projects/project1-fermat/fermat.py
+++ projects/project1-fermat/fermat.py
@@ -6,15 +6,25 @@ def prime_test(N, k):
 
 # You will need to implement this function and change the return value.
 def mod_exp(x, y, N): 
-	return 1
-	
+	if y == 0:
+		return 1
+	z = mod_exp(x, y//2, N)
+
+	if y % 2 == 0:
+		return z**2 % N
+	else:
+		return x * z**2 % N
+
+
 # You will need to implement this function and change the return value.   
 def fprobability(k):
-    return 0.0
+
+    return 1 - (1/2)**k
 
 # You will need to implement this function and change the return value.   
 def mprobability(k):
-    return 0.0
+	return 1 - (1/4)**k
+    
 
 # You will need to implement this function and change the return value, which should be
 # either 'prime' or 'composite'.
@@ -23,7 +33,16 @@ def mprobability(k):
 # random.randint(low,hi) which gives a random integer between low and
 # hi, inclusive.
 def fermat(N,k):
+	a_vals = []
+	for i in range (0, k):
+		a_vals.append(random.randint(2, N-1))
+	for a in a_vals:
+		if mod_exp(a, N-1, N) != 1:
+			return 'composite'
+		
 	return 'prime'
+
+	
 
 # You will need to implement this function and change the return value, which should be
 # either 'prime' or 'composite'.
@@ -32,4 +51,31 @@ def fermat(N,k):
 # random.randint(low,hi) which gives a random integer between low and
 #  hi, inclusive.
 def miller_rabin(N,k):
-	return 'composite'
+	a_vals = [random.randint(2, N - 1) for _ in range(k)]
+
+	for a in a_vals:
+		exp = N - 1
+
+		if N % 2 == 0:
+			return 'composite'
+		while exp % 2 == 0:
+			result = mod_exp(a, exp, N)
+			if result == N - 1:
+				break
+			if result != 1:
+				return 'composite'
+			exp //= 2
+
+	return 'prime'
+
+#test = fermat(5, 3)
+#main function to test in terminal 
+def main():
+	print(fermat(5, 3))
+	print(miller_rabin(156, 3))
+	print(mod_exp(2, 3, 5))
+	print(fprobability(5))
+	print(mprobability(5))
+
+if __name__ == "__main__":
+	main()
--- projects/project4-gene-sequencing/GeneSequencing.py
+++ projects/project4-gene-sequencing/GeneSequencing.py
@@ -35,11 +35,93 @@ class GeneSequencing:
 
 ###################################################################################################
 # your code should replace these three statements and populate the three variables: score, alignment1 and alignment2
-		score = random.random()*100;
-		alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
-			len(seq1), align_length, ',BANDED' if banded else '')
-		alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
-			len(seq2), align_length, ',BANDED' if banded else '')
+		#score = random.random()*100
+		#alignment1 = 'abc-easy  DEBUG:({} chars,align_len={}{})'.format(
+			#len(seq1), align_length, ',BANDED' if banded else '')
+		#alignment2 = 'as-123--  DEBUG:({} chars,align_len={}{})'.format(
+			#len(seq2), align_length, ',BANDED' if banded else '')
 ###################################################################################################
+		
+		if banded:
+			alignment1 = "abc"
+			alignment2 = "def"
+			a1 = seq1[:align_length]
+			a2 = seq2[:align_length]
+			table = [[0 for i in range(len(a1) + 1)] for j in range(7)]
+			for i in range(len(a1) + 1):
+				table[0][i] = i * INDEL
+			for i in range(1, 7):
+				table[i][0] = i * INDEL
+			
+
+			for i in range(1, len(a1) + 1):
+				for j in range(1, 7):
+					if a1[i - 1] == a2[j - 1]:
+						table[i][j] = min(table[i-1][j-1] + MATCH, table[i-1][j] + INDEL, table[i][j-1] + INDEL)
+					else:
+						table[i][j] = min(table[i-1][j-1] + SUB, table[i-1][j] + INDEL, table[i][j-1] + INDEL)
+			score = table[len(a1)][6]
+
+
+		else:
+			a1 = seq1[:align_length]
+			a2 = seq2[:align_length]
+
+			#build table with infinity for all values
+			table = [[0 for i in range(len(a2)+1)] for j in range(len(a1)+1)]
+
+			#main algorithm
+			for i in range(len(a1) + 1):
+				table[i][0] = i * INDEL
+			for j in range(len(a2) + 1):
+				table[0][j] = j * INDEL
+			for i in range(1, len(a1) + 1):
+				for j in range(1, len(a2) + 1):
+					if a1[i - 1] == a2[j - 1]:
+						table[i][j] = min(table[i-1][j-1] + MATCH, table[i-1][j] + INDEL, table[i][j-1] + INDEL)
+					else:
+						table[i][j] = min(table[i-1][j-1] + SUB, table[i-1][j] + INDEL, table[i][j-1] + INDEL)
+			score = table[len(a1)][len(a2)]
+
+			#backtrack to find the alignment
+			i = len(a1)
+			j = len(a2)
+			alignment1 = ""
+			alignment2 = ""
+
+			while i > 0 and j > 0:
+				if a1[i-1] == a2[j-1]:
+					alignment1 = a1[i-1] + alignment1
+					alignment2 = a2[j-1] + alignment2
+					i -= 1
+					j -= 1
+				else:
+					if table[i][j] == table[i-1][j] + INDEL:
+						alignment1 = a1[i-1] + alignment1
+						alignment2 = "-" + alignment2
+						i -= 1
+					elif table[i][j] == table[i][j-1] + INDEL:
+						alignment1 = "-" + alignment1
+						alignment2 = a2[j-1] + alignment2
+						j -= 1
+					elif table[i][j] == table[i-1][j-1] + SUB:
+						alignment1 = a1[i-1] + alignment1
+						alignment2 = a2[j-1] + alignment2
+						i -= 1
+						j -= 1
+			while i > 0:
+				alignment1 = a1[i-1] + alignment1
+				alignment2 = "-" + alignment2
+				i -= 1
+			while j > 0:
+				alignment1 = "-" + alignment1
+				alignment2 = a2[j-1] + alignment2
+				j -= 1
+
+
+			alignment1 = alignment1[:100]
+			alignment2 = alignment2[:100]
+
+		#return the results
 
 		return {'align_cost':score, 'seqi_first100':alignment1, 'seqj_first100':alignment2}
